f8274beb928e82ca1d75507577971387c0be1dd6
Leonardo HG <48033552+delta-develop@users.noreply.github.com>
2022-05-20 14:17:44 -0500

    [ADD] Dockerization of API (#9)

--- app/app.py
+++ app/app.py
@@ -10,12 +10,12 @@ from app.orchestator.orchestator import mongo_queue
 from app.routes.mutant_router import router as DNAAnalysisRouter
 from app.routes.stats_router import router as StatsRouter
 from app.settings.settings import (
-    ENVIRONMENT,
     create_collection,
     get_database_and_collection_name,
+    ENVIRONMENT,
+    EMPTYING_TIME,
 )
 
-EMPTYING_TIME = os.getenv("EMPTYING_TIME")
 
 app = FastAPI()
 app.include_router(DNAAnalysisRouter, tags=["DNAAnalysis"], prefix="/mutant")
@@ -25,18 +25,17 @@ app.include_router(StatsRouter, tags=["Statistics"], prefix="/stats")
 @app.on_event("startup")
 async def startup_event() -> None:
     """Configure the database as soon the application start up."""
-    print("Setting up database...")
     database, collection_name = get_database_and_collection_name(ENVIRONMENT)
     await create_collection(database, collection_name)
 
 
 @app.on_event("startup")
-@repeat_every(seconds=int(EMPTYING_TIME))
+@repeat_every(seconds=EMPTYING_TIME)
 async def empty_queues() -> None:
     """In order to increase performance, the data is being sent in packages,
     so when the requests traffic decrease, some results may be stucked into
     queues, for that reason, this function run periodically to empty the
     queues even if it has not reached the perfect size."""
+
     if mongo_queue.queue_size > 0:
-        print("Emptying queues...")
         await mongo_queue.empty_the_queue()
